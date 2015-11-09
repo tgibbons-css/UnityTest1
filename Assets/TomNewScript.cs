e22e222411220fbd4500865573a969339a9c19af
tgibbons-css <tgibbons@css.edu>
2015-11-09 13:50:45 -0600

    Added code to Start
    Added code to the Start method of the TomNewScript file

--- Assets/TomNewScript.cs
+++ Assets/TomNewScript.cs
@@ -5,7 +5,11 @@ public class TomNewScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
+		
+	 // Tom added this in class on Nov 9
+	 // Sample code changes here only to Start.
+	 
 	}
 	
 	// Update is called once per frame
