40f79e4a3a40c856d9b0e9ab2540de7c6e4b8c00
tgibbons-css <tgibbons@css.edu>
2015-11-12 16:55:33 -0600

    Update from the Lab at 4:55 on Nov 12
    Added a second script
    Modified the Start method in the first script

--- Assets/TomNewScript.cs
+++ Assets/TomNewScript.cs
@@ -10,6 +10,9 @@ public class TomNewScript : MonoBehaviour {
 	 // Tom added this in class on Nov 9
 	 // Sample code changes here only to Start.
 	 
+	 // Tom added this change on Nov 12 from the lab/
+	 // change 2
+	 
 	}
 	
 	// Update is called once per frame
---
+++ Assets/TomSecondScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TomSecondScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	 // Tom added this script from notepad ++
+	 // change 3
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
